81f493906d893e3bb2c0f211193a84dc870a3c15
İrem Kutnay <iremkutnay@gmail.com>
2022-04-06 11:34:43 +0300

    Initial commit

---
+++ app/src/main/java/tr/edu/maltepe/project/assignment6client/URLs.java
@@ -0,0 +1,8 @@
+package tr.edu.maltepe.project.assignment6client;
+
+public class URLs {
+    private static final String ROOT_URL = "http://192.168.1.24:8080/index";
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
+
---
+++ app/src/main/java/tr/edu/maltepe/project/assignment6client/activity_register.java
@@ -0,0 +1,14 @@
+package tr.edu.maltepe.project.assignment6client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
